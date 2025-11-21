be9efccf92e23684f94c8e6e8b47c9ab92e64c04
Prahlad721 <prajapatiprahlad721@gmail.com>
2025-11-21 17:55:38 +0530

    Auto-commit: hello.java updated

--- hello.java
+++ hello.java
@@ -4,7 +4,7 @@ public class hello{
 public static void main(Strings args[]){
     Scanner sc-new Scanner(System.in);
     int input = sc.nextInt();
-    int arr[]=new 
+    int arr[]=new int[]
 }
 
 }
